9d18fbde7fc06e6487457365f657a74bf03745e3
jurisee <45583232+jurisee@users.noreply.github.com>
2024-03-06 15:03:51 -0500

    revise config import

--- app/__init__.py
+++ app/__init__.py
@@ -6,30 +6,33 @@ from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
+
+
+app = Flask(__name__)
+
+app.config.from_pyfile('../config.py')
+app.config.from_object(Config)
 
     # Initialize Flask extensions here
-    db.init_app(app)
-    Marshmallow(app)
-    Api(app)
+db.init_app(app)
+Marshmallow(app)
+Api(app)
 
     # Register blueprints here
-    from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
 
-    from app.news import bp as news_bp
-    app.register_blueprint(news_bp, url_prefix='/news')
+from app.news import bp as news_bp
+app.register_blueprint(news_bp, url_prefix='/news')
 
-    from app.registry import bp as registry_bp
-    app.register_blueprint(registry_bp, url_prefix='/registry')
+from app.registry import bp as registry_bp
+app.register_blueprint(registry_bp, url_prefix='/registry')
 
-    from app.api import bp as api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
+from app.api import bp as api_bp
+app.register_blueprint(api_bp, url_prefix='/api')
 
-    from app.actions import bp as actions_bp
-    app.register_blueprint(actions_bp, url_prefix='/actions')
+from app.actions import bp as actions_bp
+app.register_blueprint(actions_bp, url_prefix='/actions')
 
-
-    return app
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
