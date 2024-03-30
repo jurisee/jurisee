35d1ac80ee70f5a6c1ca484dee404dedfa5ef925
jurisee <45583232+jurisee@users.noreply.github.com>
2024-03-30 18:28:24 -0400

    update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,13 @@
+import sys
+import logging
 from app import app
+
+logging.basicConfig(level=logging.Debug, filename='/var/www/html/jurisee/logs/jurisee.log',format='%(asctime)s %(message)s')
+sys.path.insert(0, 'var/www/html/jurisee')
+sys.path.insert(0, '/var/www/html/jurisee/environments/jurisee/lib/python3.10/site-packages')
+
+from jurisee.app import app as application
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host='0.0.0.0')
+    app.run()
