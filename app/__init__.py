03393a6a6a9764afd99c6fd602fcf4bb07596074
jurisee <45583232+jurisee@users.noreply.github.com>
2024-04-08 12:27:35 -0400

    update wsgi and app init

--- app/__init__.py
+++ app/__init__.py
@@ -7,28 +7,25 @@ from flask import Flask
 from flask import Blueprint
 from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
-from flask_migrate import Migrate
 
-def create_app():
-    app = Flask(__name__)
-    d = os.path.dirname(os.getcwd())
-    CONFIG_FILE = os.path.join(d, 'jurisee/config.py')
-    app.config.from_pyfile(CONFIG_FILE)
-    app.config.from_object(Config)
-    # Initialize Flask extensions here
-    db.init_app(app)
-    Marshmallow(app)
-    Api(app)
-    # Register blueprints here
-    from app.main import main_bp
-    app.register_blueprint(main_bp)
-    from app.news import bp as news_bp
-    app.register_blueprint(news_bp, url_prefix='/news')
-    from app.registry import bp as registry_bp
-    app.register_blueprint(registry_bp, url_prefix='/registry')
-    from app.api import bp as api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
-    from app.actions import bp as actions_bp
-    app.register_blueprint(actions_bp, url_prefix='/actions')
-    return app
+app = Flask(__name__)
+d = os.path.dirname(os.getcwd())
+CONFIG_FILE = os.path.join(d, 'jurisee/config.py')
+app.config.from_pyfile(CONFIG_FILE)
+app.config.from_object(Config)
+# Initialize Flask extensions here
+db.init_app(app)
+Marshmallow(app)
+Api(app)
+# Register blueprints here
+from app.main import main_bp
+app.register_blueprint(main_bp)
+from app.news import bp as news_bp
+app.register_blueprint(news_bp, url_prefix='/news')
+from app.registry import bp as registry_bp
+app.register_blueprint(registry_bp, url_prefix='/registry')
+from app.api import bp as api_bp
+app.register_blueprint(api_bp, url_prefix='/api')
+from app.actions import bp as actions_bp
+app.register_blueprint(actions_bp, url_prefix='/actions')
 
--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,4 @@
-import sys
-import logging
 from app import app
 
-logging.basicConfig(level=logging.Debug, filename='/var/www/html/jurisee/logs/jurisee.log',format='%(asctime)s %(message)s')
-sys.path.insert(0, 'var/www/html/jurisee')
-sys.path.insert(0, '/var/www/html/jurisee/environments/jurisee/lib/python3.10/site-packages')
-
-from jurisee.app import app as application
-
 if __name__ == "__main__":
-    app = create_app()
-    app.run()
+    app.run()app = create
